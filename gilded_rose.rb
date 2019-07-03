0f70beb52f97826f7f0a219cc4aeb4d948ce1171
abdullahmuhammad <AbdullahMuhammadBasra@gmail.com>
2019-07-03 23:14:02 +0500

    Modified gilded_rose for conjured items and wrote a few test cases

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -9,6 +9,7 @@ class GildedRose
         if item.quality > 0
           if item.name != 'Sulfuras, Hand of Ragnaros'
             item.quality = item.quality - 1
+            item.quality = item.quality - 1 if(item.name == 'Conjured Mana Cake' && item.quality > 0)
           end
         end
       else
@@ -37,6 +38,7 @@ class GildedRose
             if item.quality > 0
               if item.name != 'Sulfuras, Hand of Ragnaros'
                 item.quality = item.quality - 1
+                item.quality = item.quality - 1 if(item.name == 'Conjured Mana Cake' && item.quality > 0)
               end
             end
           else
--- gilded_rose_spec.rb
+++ gilded_rose_spec.rb
@@ -7,5 +7,50 @@ describe GildedRose do
       GildedRose.new(items).update_quality()
       expect(items[0].name).to eq 'foo'
     end
+
+    it 'degrades quality by 1 for regular items' do
+      items = [Item.new('Hammer of Thor', 10, 10)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to eq 9
+      expect(items[0].sell_in).to eq 9
+    end
+
+    it 'does not degrades quality or sell_in for Sulfuras' do
+      items = [
+          Item.new('Sulfuras, Hand of Ragnaros', 5, 80),
+          Item.new('Sulfuras, Hand of Ragnaros', -5, 80)
+      ]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to eq 80
+      expect(items[0].sell_in).to eq 5
+      expect(items[1].quality).to eq 80
+      expect(items[1].sell_in).to eq -5
+    end
+
+    it 'degrades quality of conjured items by 2 before sell_in' do
+      items = [Item.new('Conjured Mana Cake', 10, 10)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to eq 8
+      expect(items[0].sell_in).to eq 9
+    end
+
+    it 'degrades quality of conjured items by 4 after sell_in' do
+      items = [Item.new('Conjured Mana Cake', -1, 10)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to eq 6
+      expect(items[0].sell_in).to eq -2
+    end
+
+    it 'degrades quality but never less then 0' do
+      items = [Item.new('Conjured Mana Cake', -5, 3)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to eq 0
+      expect(items[0].sell_in).to eq -6
+
+      items = [Item.new('Hammer of Thor', 0, 1)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to eq 0
+      expect(items[0].sell_in).to eq -1
+    end
   end
 end
